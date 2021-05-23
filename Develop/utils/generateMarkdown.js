6300dc6afbdff885cec0f38b2121011f3e35bad2
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-05-22 17:31:43 -0700

    updated license TOS section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,7 +8,9 @@ function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(data) { 
+  return data.license;
+}
 
 function renderCreditsSection(data) {
   var creditsContent = '';
@@ -46,6 +48,10 @@ ${data.usage}
 ## Credits
 
 ${renderCreditsSection(data)}
+
+## License
+
+${renderLicenseSection(data)}
 `;
 }
 
